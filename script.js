2831a0a40505e9afb3291ff9fa9127d8dcb0c318
lhtn <lily.nguyen1129@gmail.com>
2022-12-01 23:17:26 -0500

    Add tempOutputScreen, condense compute function, fix event listeners for number and operation buttons

--- script.js
+++ script.js
@@ -1,151 +1,107 @@
-let currentNum = "";
-let previousNum = "";
-let operator = "";
-// let clearOnNextNum = false;
-let result;
-
 const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operation");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".delete");
 const equalsButton = document.querySelector(".equals");
 const previousOutputScreen = document.querySelector(".previous-output");
+const tempOutputScreen = document.querySelector(".temp-output");
 const currentOutputScreen = document.querySelector(".current-output");
 
-
-// Create functions for basic math operators
-function add(firstValue, secondValue) {
-    return firstValue + secondValue;
-}
-
-function subtract(firstValue, secondValue) {
-    return firstValue - secondValue;
-}
-
-function multiply(firstValue, secondValue) {
-    return firstValue * secondValue;
-}
-
-function divide(firstValue, secondValue) {
-    return firstValue / secondValue;
-}
-
-function modulo (firstValue, secondValue) {
-    return firstValue - (Math.floor(firstValue / secondValue) * secondValue);
-}
-
-
-// Calculates the answer when you click the Equals button
-function compute(previousNum, currentNum, operator) {
-    const firstValue = Number(previousNum);
-    const secondValue = Number(currentNum);
-    try {
-        switch(operator) {
-            case "+":
-                result = add(firstValue, secondValue);
-                break;
-            case "-": 
-                result = subtract(firstValue, secondValue);
-                break;
-            case "*": 
-                result = multiply(firstValue, secondValue);
-                break;
-            case "/": 
-                result = divide(firstValue, secondValue);
-                break;
-            case "%": 
-                result = modulo(firstValue, secondValue);
-                break;
-        }
-    }
-    catch(e) {
-        currentOutputScreen.textContent = ("There's an error: ", e);
-    };
-    updateScreen();
-}
-
-
-function updateScreen() {
-    currentNum = result;
-    currentOutputScreen.textContent = result;
-    // clearOnNextNum = true;
-}
-
-
-numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        appendNumber(e.target.textContent);
-    });
-});
-
-
-// Add operator to screen
-operationButtons.forEach(button => {
-    button.addEventListener("click", (e) => {
-        appendOperator(e.target.textContent);
-    });
-});
-
-
-equalsButton.addEventListener("click", () => {
-    if (currentNum != "" && previousNum != "") {
-        compute(previousNum, currentNum, operator);
-    }
-});
-
-
-clearButton.addEventListener("click", clearScreen);
-
-
-deleteButton.addEventListener("click", deleteValue);
+let previousNum = "";
+let currentNum = "";
+let operator = "";
+let result = "";
 
 
 // Add number to screen
-function appendNumber(num) {
-    // if (clearOnNextNum) {
-    //     clearOnNextNum = false;
-    //     clearScreen();
-    // }
-    if (currentNum.length <= 18) {
-        currentNum += num;
-        currentOutputScreen.textContent = currentNum;
-    }
-}
+numberButtons.forEach(number => {
+    number.addEventListener("click", (e) => {
+        if (currentNum.length <= 18) {
+            currentNum += e.target.textContent;
+            currentOutputScreen.textContent = currentNum;
+        }
+    });
+});
 
 
 // Add operator to screen
-function appendOperator(op) {
-    // clearOnNextNum = false;
-    if (currentNum != "") {
-        if (previousNum === "") {
-            operator = op;
-            previousNum = currentNum;
-            currentNum = "";
-            previousOutputScreen.textContent = previousNum + " " + operator;
-            currentOutputScreen.textContent = "";
-        } else if (previousNum != "") {
-            operator = op;
-            previousNum = previousNum + operator + currentNum;
-            currentNum = "";
-            previousOutputScreen.textContent = previousNum + " " + operator + " " + currentNum;
+operationButtons.forEach(operation => {
+    operation.addEventListener("click", (e) => {
+        if (previousNum === "" && currentNum === "") return;
+        if (previousNum && currentNum && operator) {
+            compute();
+        } else {
+            result = Number(currentNum);
         }
-    }
+        handleNumAndOp(e.target.textContent);
+        operator = e.target.textContent;
+    });
+});
+
+
+function handleNumAndOp(value = "") {
+    previousNum += currentNum + " " + value + " ";
+    previousOutputScreen.textContent = previousNum;
+    currentNum = "";
+    currentOutputScreen.textContent = "";
+    tempOutputScreen.textContent = result;
 }
+
+
+equalsButton.addEventListener("click", () => {
+    if (previousNum === "" || currentNum === "") return;
+    compute();
+    handleNumAndOp();
+    previousNum = "";
+    currentNum = result;
+    currentOutputScreen.textContent = result;
+    tempOutputScreen.textContent = "";
+});
 
 
 // Clears the display when you click the Clear button
-function clearScreen() {
-    currentNum = "";
+clearButton.addEventListener("click", () => {
     previousNum = "";
+    currentNum = "";
     operator = "";
-    currentOutputScreen.textContent = "";
+    result = "";
     previousOutputScreen.textContent = "";
-}
+    currentOutputScreen.textContent = "";
+    tempOutputScreen.textContent = "";
+});
 
 
 // Deletes one value when you click the Delete button
-function deleteValue() {
+deleteButton.addEventListener("click", () => {
     currentNum = currentNum.slice(0, -1);
     currentOutputScreen.textContent = currentOutputScreen.textContent.slice(0, -1);
-}
+});
 
+
+// Calculates the answer when you click the Equals button
+function compute() {
+    result = Number(result);
+    currentNum = Number(currentNum);
+    try {
+        switch(operator) {
+            case "+":
+                result = result + currentNum;
+                break;
+            case "-": 
+                result = result - currentNum;
+                break;
+            case "X": 
+                result = result * currentNum;
+                break;
+            case "/": 
+                result = result / currentNum;
+                break;
+            case "%": 
+                result = result - (Math.floor(result / currentNum) * currentNum);
+                break;
+        }
+    }
+    catch {
+        currentOutputScreen.textContent = ("There's an error: ");
+    };
+}
